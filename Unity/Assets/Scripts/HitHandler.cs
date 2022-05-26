7faa45265de2de58077815a5d06f8b5c4c90f3ab
3103-David <davigalih@gmail.com>
2022-05-26 18:16:37 +0700

    Update

---
+++ Unity/Assets/Scripts/HitHandler.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class HitHandler : MonoBehaviour
+{
+    public float AttackPoint;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && collision.isTrigger)
+        {
+            var player = collision.GetComponent<PlayerActive>();
+            player.HealthPoint -= AttackPoint;
+        }
+    }
+}
